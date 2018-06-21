31b7dcc2c215227cc3979f708e42f6215b0a9cc7
Pierre <38044540+PierreCASTAINGv@users.noreply.github.com>
2018-06-21 10:17:01 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function rondeau () {
+             document.getElementById("b-jambe").style.transform = "rotate(720deg)";
+             document.getElementById("b-jambe").style.animation = "3s";
+             document.getElementById("b-jambe").style.background = "red";
+}
